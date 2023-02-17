f50b49e1b94947603fb02ef9634066b8beef9269
Austin Atchley <austinatchley@gmail.com>
2023-02-16 23:02:43 -0800

    Add lambda dir and source code from the existing lambda

---
+++ lambda/index.js
@@ -0,0 +1,25 @@
+// This code represents a Lambda that is deployed to the Edge to resolve request paths coming from CloudFront. We modify the URL to point to the correct file in S3 static storage
+
+'use strict';
+exports.handler = (event, context, callback) => {
+
+    // Extract the request from the CloudFront event that is sent to Lambda@Edge 
+    var request = event.Records[0].cf.request;
+
+    // Extract the URI from the request
+    var olduri = request.uri;
+
+    // Match any '/' that occurs at the end of a URI. Replace it with a default index
+    var newuri = olduri.replace(/\/$/, '\/index.html');
+
+    // Log the URI as received by CloudFront and the new URI to be used to fetch from origin
+    console.log("Old URI: " + olduri);
+    console.log("New URI: " + newuri);
+
+    // Replace the received URI with the URI that includes the index page
+    request.uri = newuri;
+
+    // Return to CloudFront
+    return callback(null, request);
+
+};
