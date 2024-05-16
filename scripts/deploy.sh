f453ea9c16dae178ee31496c4a26f0f726ae1e79
Austin Atchley <austinatchley@gmail.com>
2024-05-15 22:39:02 -0400

    Fix deploy script naming

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,3 @@
-BUCKET_NAME="staticsitebucket-austinatchleyaws"
-
 rm -R public/*
 hugo
-
-aws s3 rm s3://$BUCKET_NAME --recursive
-aws s3 cp public s3://$BUCKET_NAME/ --recursive
+hugo deploy
--- scripts/hugo_deploy.sh
+++
@@ -1,3 +0,0 @@
-rm -R public/*
-hugo
-hugo deploy
